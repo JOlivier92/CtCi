fc145b9d5d23ceb440e7c3ef9f011c05bb91dba4
Joseph Olivier <josepholivier92@gmail.com>
2019-01-05 23:39:04 -0800

    start binary trees

---
+++ binaryTrees/randomTreePractice.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# this is not related to CTCI, but personal practice
+
--- linkedLists/removeDups.py
+++ linkedLists/removeDups.py
@@ -2,14 +2,17 @@ class SinglyLinkedList:
     def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
-        
-    def next(self):
+
+    def get_next(self):
         return self.next
+
+    def set_next(self,new_next):
+        self.next = new_next
 
 def removeDups(ll):
     alreadyPresent = {}
     currNode = ll
     nextNode = ll.next()
 
-    while currNode.next:
+    while currNode.get_next:
         if currNode.val in alreadyPresent:
