5032692448654f6884b840c7a6916ba73ed3cf6a
Joseph Olivier <josepholivier92@gmail.com>
2019-01-23 23:15:11 -0800

    squashed push

--- binaryTrees/randomTreePractice.py
+++ binaryTrees/randomTreePractice.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # this is not related to CTCI, but personal practice
 
+class Node:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.value = value
+    
